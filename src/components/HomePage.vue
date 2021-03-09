39e37c30f0a3493e4d0ff44c263322daaafd6935
SHombert <sarah.hombert@gmail.com>
2021-03-09 16:52:42 +0100

    refactor sans bootstrapvue (ça marche po)

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,12 @@
 <template>
- <div></div>
+ <div>
+   <h1>Test</h1></div>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
-  props: {
-    msg: String
-  }
+  
 }
 </script>
 
--- src/components/MenuView.vue
+++ src/components/MenuView.vue
@@ -1,74 +1,11 @@
 <template>
-  <b-navbar  role="navigation" aria-label="main navigation">
-     <b-nav-item href="#">Link</b-nav-item>
-    <div class="b-navbar-brand">
-      <a class="b-navbar-item animated" href="/">
-        
-      </a>
-
-      <a
-        role="button"
-        class="b-navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="b-navbarBasicExample"
-        :class="{ 'is-active': mobileMenuActive }"
-        @click="mobileMenuActive = !mobileMenuActive"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div class="b-b-navbar-menu" :class="{ 'is-active': mobileMenuActive }">
-      <div class="b-navbar-start">
-        <router-link class="b-navbar-item" to="/"> Accueil </router-link>
-
-        <div class="b-navbar-item has-dropdown is-hoverable">
-          <a class="b-navbar-link">
-            Composteurs
-          </a>
-
-          <div class="b-navbar-dropdown">
-            <router-link class="b-navbar-item" to="/composters">
-              Liste
-            </router-link>
-            <a class="b-navbar-item">
-              Carte
-            </a>
-            <hr class="b-navbar-divider" />
-            <router-link class="b-navbar-item" to="/about">
-              A propos
-            </router-link>
-            <a
-              class="b-navbar-item"
-              href="https://github.com/alexsedlex/org.univ.compostmap"
-              target="github"
-            >
-              Voir le code source
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="b-navbar-end">
-        <div class="b-navbar-item copyright">
-          Created by Univ-Nantes &copy;
-        </div>
-      </div>
-    </div>
-  </b-navbar>
+  <div></div>
 </template>
 
 <script>
 export default {
   name: "MenuView",
-  data() {
-    return {
-      mobileMenuActive: false,
-    };
-  },
+
 };
 </script>
 
